7f72bd754299c9314f5421dbe291c83b313414a9
eeve <eeve@users.noreply.github.com>
2020-12-23 13:42:17 +0800

    improve

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-cat "$MC_SK" >> /root/.ssh/authorized_keys
+echo "$1" >> /root/.ssh/authorized_keys
 sed -i 's/#StrictModes no/StrictModes yes/g' /etc/ssh/sshd_config
 service sshd restart
