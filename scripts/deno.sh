8d53cbca5982ab047a0582fd20abf6ddccaff4c6
eeve <eeve@users.noreply.github.com>
2020-12-23 12:13:37 +0800

    improve

--- scripts/deno.sh
+++ scripts/deno.sh
@@ -2,4 +2,4 @@
 #!/bin/bash
 
 curl -fsSL https://deno.land/x/install/install.sh | sh
-nohup DENO_INSTALL="$HOME/.deno" PATH="$DENO_INSTALL/bin:$PATH" deno run --allow-net mod.ts > serve.log 2>&1 &
+nohup $HOME/.deno/bin/deno run --allow-net mod.ts > serve.log 2>&1 &
