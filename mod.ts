4b8cadfa0b79f93df9414bdba1801be1a3c4e8c8
eeve <eeve@users.noreply.github.com>
2020-12-23 13:49:41 +0800

    improve

--- mod.ts
+++ mod.ts
@@ -2,10 +2,10 @@ import { serve } from "https://deno.land/std@0.82.0/http/server.ts";
 const s = serve({ hostname: '0.0.0.0', port: 8000 })
 console.log("http://0.0.0.0:8000/");
 for await (const req of s) {
+  const headers = new Headers();
+  headers.append('content-type', 'text/html; charset=utf-8')
   req.respond({
     body: "Hello World\n",
-    headers: {
-      'content-type': 'text/html; charset=utf-8'
-    }
+    headers
   })
 }
