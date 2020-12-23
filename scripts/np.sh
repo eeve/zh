f328d9e47b92d0c8933f8752acbec61135ea8010
eeve <eeve@users.noreply.github.com>
2020-12-23 12:07:58 +0800

    improve

--- scripts/np.sh
+++ scripts/np.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
+wget https://github.com/ehang-io/nps/releases/download/v0.26.9/linux_amd64_client.tar.gz
+tar -zxvf linux_amd64_client.tar.gz
+chmod +x npc
+rm -rf linux_amd64_client.tar.gz
+
 nohup ./npc -server="$NP_SERVER" -vkey="$NP_SK" -type=tcp > npc.log 2>&1 &
