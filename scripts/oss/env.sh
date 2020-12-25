af132fba4489ade1a5458847dc6768197e96f08d
eeve <eeve@users.noreply.github.com>
2020-12-25 12:01:37 +0800

    test 1

--- scripts/oss/env.sh
+++ scripts/oss/env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 apt install s3fs -y
-echo "$1" >> $HOME/.passwd-s3fs
+echo "$1" >> ./passwd-s3fs
 mkdir temp
-s3fs temp temp -o url=$2 -o passwd_file=$HOME/passwd-s3fs
+s3fs temp temp -o url=$2 -o passwd_file=./passwd-s3fs
