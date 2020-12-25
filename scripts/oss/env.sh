ec90928b268e2d7c8d5db8a5409a7e74a2fda898
eeve <eeve@users.noreply.github.com>
2020-12-25 13:44:21 +0800

    test 1

--- scripts/oss/env.sh
+++ scripts/oss/env.sh
@@ -4,4 +4,4 @@ echo "$1" > ./passwd-s3fs
 chmod 600 ./passwd-s3fs
 mkdir temp
 chown -R runner:docker temp
-s3fs temp temp -o url=$2 -o passwd_file=./passwd-s3fs
+s3fs temp temp -o url=$2 -o passwd_file=./passwd-s3fs -o dbglevel=info -f -o curldbg
